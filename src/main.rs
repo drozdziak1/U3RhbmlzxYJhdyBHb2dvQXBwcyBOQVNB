52f27f3f4c4dafca5404b1873b9b119adb3a9395
Stan Drozd <drozdziak1@gmail.com>
2021-01-09 17:00:03 +0100

    Add actix-web, Initial dummy app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+/// Retrieves a collection of pictures from within the specified date range
+async fn pictures(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/pictures", web::get().to(pictures))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
